5d99c88c6e0e78d14698edd99c75b2aec448a1e0
rawGem <sj.browne@gatech.edu>
2014-11-19 20:57:46 -0500

    creates a user with a role attribute in profile

--- client/subs.js
+++ client/subs.js
@@ -1,4 +1,11 @@
 // change second argument to "pupil" to get 
 // a different cursor back from the db
-
+Accounts.createUser({
+  'username': 'Dr. Jones', 
+  'email': 'indianajones@oxford.edu',
+  'password': 'password',
+  'profile': {
+    'role':'admin'
+  }
+})
 Meteor.subscribe("lessons", "admin") 
