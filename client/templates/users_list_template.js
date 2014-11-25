81a706d3e5af5cf085d56f5f94abe299b3bcbb17
rawGem <sj.browne@gatech.edu>
2014-11-24 21:11:25 -0500

    looks like I'm protected from removing the current user from th users cursor; or it's already on the client

--- client/templates/users_list_template.js
+++ client/templates/users_list_template.js
@@ -1,11 +1,11 @@
 
 
-Template.usersList.helpers({
-  // try doing all of the filtering
-  // on the client, get it working,
-  // then try to tackle working around
-  // Accounts' autopublish 
-  users: function() {
-    return Meteor.users.find()
-  }
-})
+//Template.usersList.helpers({
+//  // try doing all of the filtering
+//  // on the client, get it working,
+//  // then try to tackle working around
+//  // Accounts' autopublish 
+//  users: function() {
+//    return Meteor.users.find()
+//  }
+//})
--- server/pubs.js
+++ server/pubs.js
@@ -17,9 +17,12 @@ Meteor.publish("users_for_admin", function(user) {
   var loggedInUser = Meteor.users.findOne({_id:this.userId});
   if (loggedInUser && loggedInUser.profile.hasOwnProperty("role")) {
     var firstEmail = _.first(loggedInUser.emails)
+    console.log(firstEmail.address)
     if (loggedInUser.profile.role === "admin") {
       var firstEmail = _.first(loggedInUser.emails)
+      //var allUsers = Meteor.users.find({username: "Dr. Jones" })
       var allUsers = Meteor.users.find({"emails.address": {$ne: firstEmail.address} })
+      console.log(allUsers.count())
       return allUsers
       //return Meteor.users.find();
     }
