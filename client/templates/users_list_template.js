d9510d0b5b609eabc97031a6c21261987abef815
rawGem <sj.browne@gatech.edu>
2014-11-24 21:16:19 -0500

    this works, not sure if it's good

--- client/templates/users_list_template.js
+++ client/templates/users_list_template.js
@@ -1,11 +1,13 @@
 
 
-//Template.usersList.helpers({
-//  // try doing all of the filtering
-//  // on the client, get it working,
-//  // then try to tackle working around
-//  // Accounts' autopublish 
-//  users: function() {
-//    return Meteor.users.find()
-//  }
-//})
+Template.usersList.helpers({
+  // try doing all of the filtering
+  // on the client, get it working,
+  // then try to tackle working around
+  // Accounts' autopublish 
+  users: function() {
+    console.log(Meteor.userId())
+    var removeCurrentUser = {$ne: Meteor.userId()}
+    return Meteor.users.find({_id: removeCurrentUser })
+  }
+})
