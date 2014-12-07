e8b964fed1d41d677481930a3e85d502115b32be
rawGem <sj.browne@gatech.edu>
2014-12-07 15:14:10 -0500

    cleanup

--- client/templates/user.js
+++ client/templates/user.js
@@ -12,41 +12,20 @@ Template.usersList.helpers({
   }
 })
 
-//Template.userItem.helpers({
-//  circle: function() {
-//    var selector = "#" + this._id
-//    var svg = d3.select(selector).append("svg")
-//    svg.attr("width", 100).attr("height", 100)
-//    var circle = svg.selectAll("circle")
-//                    .data([0])
-//                    .enter()
-//                    .append("circle");
-//    circle.attr("r", 10)
-//          .attr("cx", 20)
-//          .attr("cy", 20)
-//          .attr("fill", this.profile.status)
-//    var circleText = svg.selectAll("text")
-//                        .data([0])
-//                        .enter()
-//                        .append("text")
-//    circleText.text(_.first(this.emails).address)
-//              .attr("x", 20)
-//              .attr("y", 20)
-//  }
-//})
-
 Template.usersList.events({
   'click': function() {
     return console.log(this._id)
   }
 })
 
+
+
+
 Template.userItem.helpers({
   email: function() {
     return _.first(this.emails).address
   }
 })
-
 
 Template.userItem.rendered = function() {
   var id = this.data._id
@@ -79,9 +58,3 @@ Template.userItem.rendered = function() {
     $( document ).tooltip();
   });
 }
-
-//Template.userItem.rendered = function() {
-//  $(function() {
-//    $( document ).tooltip();
-//  });
-//}
