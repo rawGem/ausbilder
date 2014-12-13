5e7f982e42240da6a6d3bc4ffcc7b271f9f63ad2
rawGem <sj.browne@gatech.edu>
2014-12-13 10:46:03 -0500

    only need one d3 render

--- client/templates/admin.js
+++ client/templates/admin.js
@@ -35,34 +35,34 @@ Template.userItem.helpers({
   }
 })
 
-Template.userItem.rendered = function() {
-  var id = this.data._id
-  var status = this.data.profile.status 
-  var address = _.first(this.data.emails).address
-
-  var selector = "#" + id 
-  var svg = d3.select(selector).append("svg")
-  svg.attr("width", 50).attr("height", 50)
-  var circle = svg.selectAll("circle")
-                  .data([0])
-                  .enter()
-                  .append("circle");
-  circle.attr("r", 10)
-        .attr("cx", 20)
-        .attr("cy", 20)
-  var circleText = svg.selectAll("text")
-                      .data([0])
-                      .enter()
-                      .append("text")
-  circleText.text(address)
-            .attr("x", 20)
-            .attr("y", 20)
-
-  this.autorun(function() {
-   circle.attr("fill", status) 
-  })
-
-  $(function() {
-    $( document ).tooltip();
-  });
-}
+//Template.userItem.rendered = function() {
+//  var id = this.data._id
+//  var status = this.data.profile.status 
+//  var address = _.first(this.data.emails).address
+//
+//  var selector = "#" + id 
+//  var svg = d3.select(selector).append("svg")
+//  svg.attr("width", 50).attr("height", 50)
+//  var circle = svg.selectAll("circle")
+//                  .data([0])
+//                  .enter()
+//                  .append("circle");
+//  circle.attr("r", 10)
+//        .attr("cx", 20)
+//        .attr("cy", 20)
+//  var circleText = svg.selectAll("text")
+//                      .data([0])
+//                      .enter()
+//                      .append("text")
+//  circleText.text(address)
+//            .attr("x", 20)
+//            .attr("y", 20)
+//
+//  this.autorun(function() {
+//   circle.attr("fill", status) 
+//  })
+//
+//  $(function() {
+//    $( document ).tooltip();
+//  });
+//}
